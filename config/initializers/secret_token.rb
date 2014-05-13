97a5e998d65980bc5e64a0f8dbf49ac0907f5608
adeng21 <adeng21@gmail.com>
2014-05-13 11:39:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopperista::Application.config.secret_key_base = '0a743b4a22b75e42a08063ec3bc4cb0963abb322b31e1b1b24e491b60af582da7442b0b93d7c45bdf3d8e22a8ec9d9f5eeee1a0f2b172943e46d091e8f4f1e5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shopperista::Application.config.session_store :cookie_store, key: '_Shopperista_session'
