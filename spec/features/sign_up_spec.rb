3cbc6e8df1a5f32a7d76e24f849b9c3cbfac295b
laransom <laransom@gmail.com>
2014-05-13 16:21:35 -0400

    added failing tests and user index root path

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,20 +5,36 @@ feature 'user signs up' , %Q{
   I want to sign up
   so that I can submit and review site content
   } do
+
+    before :each do
+      visit new_user_registration_path
+    end
+
     context 'user succesfully signs in' do
       let(:user){ FactoryGirl.build(:user) }
 
       scenario 'specifying valid and required information' do
-        visit new_user_registration_path
+
+        pre_count = User.count
+
         fill_in 'Name', with: user.name
         fill_in 'Email', with: user.email
         fill_in 'user_password', with: user.password
         fill_in 'Password confirmation', with: user.password
         click_on 'Sign up'
-        save_and_open_page
 
         expect(page).to have_content("You're in!")
-        expect(page).to have_content("Sign Out")
+        expect(User.count).to eq(pre_count + 1)
       end
+    end
+
+    scenario 'required information is not supplied' do
+
+      pre_count = User.count
+
+      click_button 'Sign up'
+
+      save_and_open_page
+      expect(page).to have_content("can't be blank")
     end
   end
