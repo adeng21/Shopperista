e85c28b2d18c799f201686886a80da702ade2901
adeng21 <adeng21@gmail.com>
2014-05-13 17:10:49 -0400

    generated outfit, pant, shirt, shoe models and set up associations

---
+++ db/migrate/20140513210727_add_user_i_dto_outfit.rb
@@ -0,0 +1,5 @@
+class AddUserIDtoOutfit < ActiveRecord::Migration
+  def change
+    add_column :outfits, :user_id, :integer
+  end
+end
---
+++ spec/factories/pants.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :pant do
+  end
+end
