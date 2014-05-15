5f6448594d9138de8021822334aacf308258f9ad
Greg Towne <townegr@yahoo.com>
2014-05-14 22:31:35 -0400

    module for build_product_struct -- errors

--- app/models/pant.rb
+++ app/models/pant.rb
@@ -2,18 +2,19 @@ require 'open-uri'
 require 'json'
 require "#{Rails.root}/lib/modules/shop.rb"
 
+
 class Pant < ActiveRecord::Base
   has_many :outfits
   PANTS = "http://api.shopstyle.com/api/v2/products?pid=uid4976-25022176-34&cat=mens-pants&limit=100"
 
   include Shop
 
+  # def retrieve_mens_pants_general
+  #   load_json_file(PANTS)
+  # end
+
   def retrieve_mens_pants
-    load_json_file(PANTS)
-  end
-
-  def get_pant_url
-
+    build_product_struct(PANTS)
   end
 
   def get_brand_name
